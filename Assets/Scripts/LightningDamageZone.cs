09af874d938c8e6619ec0c6c34be56251b20f2ee
Raekar <marcrichter-stade@web.de>
2018-02-25 05:37:53 +0100

    Implemented lightning damage

---
+++ Assets/Scripts/LightningDamageZone.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LightningDamageZone : MonoBehaviour {
+
+    public bool isDamaging;
+    public float damage = 20;
+
+    private void OnTriggerEnter(Collider col)
+    {
+        if (col.tag == "Player")
+        {
+            col.SendMessage((isDamaging) ? "TakeDamage" : "Heal", damage);
+        }
+    }
+}
